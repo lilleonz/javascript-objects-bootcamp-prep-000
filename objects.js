f1ac818d87bbb3c3bcf7629a8f0c518bca443e68
Leon Little <little-leon@hotmail.com>
2018-02-27 19:55:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ var updatePlaylist = function (playlist, artistName, songTitle) {
   return playlist;
 };
 
+var removeFromPlaylist = function (playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+};
